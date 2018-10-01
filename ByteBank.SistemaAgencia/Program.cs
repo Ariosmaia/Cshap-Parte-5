f4ca1b3db98e7948d8db61c3d3bed6934e1e185c
Allan Rios <ariosmaia@yahoo.com.br>
2018-10-01 18:22:08 -0300

    DataTime e Humanizer

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ByteBank.Modelos;
 using ByteBank.Modelos.Funcionarios;
+using Humanizer;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -14,13 +15,27 @@ namespace ByteBank.SistemaAgencia
         {
             ContaCorrente conta = new ContaCorrente(847, 489754);
             
-            FuncionarioAutenticavel carlos = null;
-            carlos.Autenticar("dfsdfsfsfsd");
-
+          
 
             Console.WriteLine(conta.Numero);
 
+            DateTime dataFimPagamento = new DateTime(2018, 10, 17);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan difenreca = TimeSpan.FromMinutes(40); //dataFimPagamento - dataCorrente;
+
+            Console.WriteLine(dataCorrente);
+            Console.WriteLine(dataFimPagamento);
+
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(difenreca);
+            Console.WriteLine(mensagem);
+            
             Console.ReadLine();
         }
+
+
+        
+        }
     }
-}
+
